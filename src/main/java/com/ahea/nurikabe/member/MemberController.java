3572838b81907348c2de0bd65ab331e649752f6b
moong <gmun0929@gmail.com>
2018-09-09 00:54:27 +0900

    test

--- src/main/java/com/ahea/nurikabe/member/MemberController.java
+++ src/main/java/com/ahea/nurikabe/member/MemberController.java
@@ -4,13 +4,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/member")
 public class MemberController {
 
     private final MemberService memberService;
-
+    
     // HTTP post get put delete
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
