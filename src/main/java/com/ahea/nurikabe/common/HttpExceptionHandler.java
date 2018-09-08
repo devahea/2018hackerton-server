3dd30962540edc013c3e2a76eae9d3c73328cc1d
Minsoo <viviennes7@naver.com>
2018-09-08 18:30:28 +0900

    puzzle 기본 틀 잡기

---
+++ src/main/java/com/ahea/nurikabe/common/HttpExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.ahea.nurikabe.common;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseBody
+@ControllerAdvice
+public class HttpExceptionHandler {
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception e) {
+        return e.getMessage();
+    }
+}
